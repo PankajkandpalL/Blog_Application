991e69e7c252e8d8356124c122127543e41b45ab
PankajkandpalL <pankajkandpal5602@gmail.com>
2022-10-09 20:58:31 +0530

    setup api with mongodb

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,38 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connect } from 'mongoose';
+import { Post } from './../../Model/Post';
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from '../../lib/mongoConnect'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+  res: NextApiResponse<Data> ) {
+
+    if(req.method !== "POST")
+    {
+      const x: any = "Only Post is applicable in this route!"
+      return res.status(401).send(x)
+    }
+
+    const { title } = req.body
+
+    console.log(title);
+    
+
+    try{
+
+      connectToDatabase()
+      const data : any = await Post.create({ title :  "John Doe" })
+      const y : any = "YUP"
+      res.send(y)
+
+    }
+    catch(e : any){
+      console.log("error", e)
+      res.status(500).send(e)
+    }
+
 }
