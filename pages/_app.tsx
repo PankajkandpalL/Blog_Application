2438441f34288e958d17a1208b2795395f800151
PankajkandpalL <pankajkandpal5602@gmail.com>
2022-10-10 10:56:06 +0530

    next-auth setup completed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import type { AppProps } from 'next/app'
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
-
-export default MyApp
